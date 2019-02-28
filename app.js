810fd3f283eddedb7b1a17f03c83d2e2dc1cd2cc
Syed Jamal <syed.jamal.1@stonybrook.edu>
2019-02-28 09:12:40 -0500

    home page changed

--- app.js
+++ app.js
@@ -1,8 +1,19 @@
 var express = require('express');
 
 var app = express();
+const path = require('path');
+const moment = require('moment');
+const bodyParser= require('body-parser');
+
+app.use(express.static(__dirname + 'public'));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.locals.pretty = true;
+
 app.get('/', function(req, res){
-   res.send("Hello World!");
+   // res.send("Hello World!");
+   res.sendFile(path.join(__dirname + '/index.html'));
 });
 app.post('/login', (req, res)=>{
    console.log(req.body.username);
