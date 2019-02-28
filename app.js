d1804c943debcb983da0e3e8ac67176b5678c003
Syed Jamal <syed.jamal.1@stonybrook.edu>
2019-02-28 08:53:46 -0500

    updated front end

--- app.js
+++ app.js
@@ -4,4 +4,7 @@ var app = express();
 app.get('/', function(req, res){
    res.send("Hello World!");
 });
+app.post('/login', (req, res)=>{
+   console.log(req.body.username);
+});
 app.listen(8080, '192.168.122.14');
