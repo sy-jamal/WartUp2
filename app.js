3011af43f8c5b69f64fb89cf2e0514d15b7c5bc6
Syed Jamal <syed.jamal.1@stonybrook.edu>
2019-02-28 20:22:13 -0500

    mongodb connected

--- app.js
+++ app.js
@@ -4,6 +4,14 @@ var app = express();
 const path = require('path');
 const moment = require('moment');
 const bodyParser= require('body-parser');
+var mongoose = require('mongoose');
+var mongoDB = 'mongodb://127.0.0.1/my_database';
+mongoose.connect(mongoDB, {useNewUrlParser});
+
+mongoose.Promise = global.Promise;
+var db= mongoose.connection;
+
+db.on('error', console.error.bind(conlose,'MongoDB connection error'));
 
 app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -11,11 +19,42 @@ app.use(bodyParser.json());
 
 app.locals.pretty = true;
 
+let UserModel = require('./public/models/user')
+
+function makeid() {
+   var text = "";
+   var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+ 
+   for (var i = 0; i < 5; i++)
+     text += possible.charAt(Math.floor(Math.random() * possible.length));
+ 
+   return text;
+ }
+
+
 app.get('/', function(req, res){
    // res.send("Hello World!");
    res.sendFile(path.join(__dirname + '/public/index.html'));
 });
 app.post('/login', (req, res)=>{
    console.log(req.body.username);
+});
+app.post('/adduser',(req, res)=>{
+   let newUser = new UserModel({
+      username: req.body.username,
+      password: req.body.password,
+      email: req.body.email,
+      varified: false,
+      key: 125325
+
+   });
+
+   newUser.save()
+   .then(doc=>{
+      console.log(doc)
+   })
+   .catch(err=>{
+      console.error(err)
+   })
 });
 app.listen(8080, '192.168.122.14');
--- public/models/user.js
+++ public/models/user.js
@@ -1,1 +1,12 @@
+const mongoose = require('mongoose');
+const Scheme = mongoose.Schema;
 
+let UserSchema = new Schema({
+    username: {type: String, required :true, max: 100},
+    password: {type: String, required :true, max: 100},
+    email: {type: String, required :true, max: 100},
+    varified: {type: Boolean, required :true, max: 100},
+    key: {type: String, required :true, max: 100}
+});
+
+module.exports = mongoose.model('User', UserSchema);
