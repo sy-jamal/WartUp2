9b68f10e3de2881e4a5c0c2f21538ab2ce89be9b
Syed Jamal <syed.jamal.1@stonybrook.edu>
2019-03-01 20:44:05 -0500

    unique validator

--- public/models/user.js
+++ public/models/user.js
@@ -2,11 +2,11 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 let UserSchema = new Schema({
-    username: {type: String, required :true, max: 100},
+    username: {type: String, required :true, max: 100, unique: true },
     password: {type: String, required :true, max: 100},
-    email: {type: String, required :true, max: 100},
+    email: {type: String, required :true, max: 100, unique: true},
     varified: {type: Boolean, required :true, max: 100},
     key: {type: String, required :true, max: 100}
 });
-
+UserSchema.plugin(uniqueValidator);
 module.exports = mongoose.model('User', UserSchema);
