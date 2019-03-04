d693ec36b7f545a2d2a131b30bc768c2e41728bf
Syed Jamal <syed.jamal.1@stonybrook.edu>
2019-03-04 09:49:55 -0500

    game model schema added to user.js

--- public/models/user.js
+++ public/models/user.js
@@ -2,16 +2,23 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 var uniqueValidator = require('mongoose-unique-validator');
 
+let GameSchema = new Schema({
+    start_date: {type: Date,  default: Date.now},
+    grid: [Number],
+    winner:{type: String, required :true, max: 3}
+});
+
 let UserSchema = new Schema({
     username: {type: String, required :true, max: 100, unique: true },
     password: {type: String, required :true, max: 100},
     email: {type: String, required :true, max: 100, unique: true},
     verified: {type: Boolean, required :true, max: 100},
     key: {type: String, required :true, max: 100},
+    totalGame:{type: Number, default: 0, required: true},
     human: {type: Number, default: 0, required: true},
     worp: {type: Number, default: 0, required: true},
     tie: {type: Number, default: 0, required: true},
-    gameList: {type: Schema.ObjectId, ref: 'Game'}
+    gameList: [GameSchema]
 });
 UserSchema.plugin(uniqueValidator);
 module.exports = mongoose.model('User', UserSchema);
