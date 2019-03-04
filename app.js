6440b653a21139aa36389590d2acffa48259acdf
Syed Jamal <syed.jamal.1@stonybrook.edu>
2019-03-04 18:25:25 -0500

    save modified

--- app.js
+++ app.js
@@ -256,48 +256,47 @@ app.post('/ttt/play', (req, res) => {
    {
       // var game_finalize= {grid: req.session.board, winner: w};
       UserModel.findOne({ email: req.session.user.email })
-               .then(doc=>{
-                  console.log("writing game update to db");
-                  doc.totalGames= doc.totalGames+1;
-                  if(w ==='X')
-                  {
-                     doc.human= doc.human+1;
-                  }
-                  else if(w==='O')
-                  {
-                     doc.wopr=doc.wopr+1;
-                  }
-                  else
-                  {
-                     doc.tie= doc.tie+1;
-                  }
-                  console.log(g)
-                  doc.gameList.push({grid: g, winner: w});
-                  console.log(doc.gameList);
-                  doc.save()
-                  .then(msg=>{
-                     console.log('store game to database');
-                  })
-                  .catch(err=>{
-                     return res.send({status: "Error", message:'Could not save game after winner is found' });
-                  })
-               })
-               .catch(err=>{
-                  return res.send({status: "ERROR", message:'Trouble finding in database'});
-               })
+         .then(doc=>{
+            console.log("writing game update to db");
+            doc.totalGames= doc.totalGames+1;
+            if(w ==='X')
+            {
+               doc.human++;
+            }
+            else if(w==='O')
+            {
+               doc.wopr++;
+            }
+            else
+            {
+               doc.tie++;
+            }
+            console.log(g)
+            doc.gameList.push({grid: g, winner: w});
+            console.log(doc.gameList);
+            doc.save()
+            .then(msg=>{
+               console.log('store game to database');
+               console.log("resetting board");
+               req.session.board= [" "," "," "," "," "," "," "," "," "];   //setting the session grid to be an empty grid
+               console.log(req.session.board);
+               return res.send({status: "OK", grid: g, winner:w});     
+            })
+            .catch(err=>{
+               console.log(err);
+               return res.send({status: "Error", message:'Could not save game after winner is found' });
+            })
+         })
+         .catch(err=>{
+            console.log(err);
+            return res.send({status: "ERROR", message:'Trouble finding in database'});
+         })
          
-      console.log("resetting board");
-      req.session.board= [" "," "," "," "," "," "," "," "," "];   //setting the session grid to be an empty grid
-      console.log(req.session.board);
+      
    }
-   console.log("sending from end");
-   if(w!="")
-   {
-      return res.send({status: "OK", grid: g, winner:w});      
-   }
-   else{
-      return res.send({status: "OK", grid: g}); 
-   }
+   console.log("sending from end");   
+   return res.send({status: "OK", grid: g}); 
+  
 	
 });
 
